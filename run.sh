1276c0426ef362c777e232f0ed2c1b002935f97d
ben kim <ben@astera.io>
2019-11-13 06:45:48 +0000

    cosr check set-to false

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/eatplus/eatplus.chatbot
-sudo sh /home/eatplus/eatplus.chatbot/migrate.sh
-sudo python /home/eatplus/eatplus.chatbot/manage.py runserver 0.0.0.0:8000
+cd /home/eatplus/eatple.chatbot
+sudo sh /home/eatplus/eatple.chatbot/migrate.sh
+sudo python /home/eatplus/eatple.chatbot/manage.py runserver 0.0.0.0:8000
