8539bc514019a0cb8533b1aecc5b061e81c2caa2
Bart Kim <bart@eatple.com>
2019-12-02 17:31:10 +0000

    static file add

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,8 +138,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 
 
 # COOKIES
--- config/settings/debug.py
+++ config/settings/debug.py
@@ -144,11 +144,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 
 # COOKIES
 #
