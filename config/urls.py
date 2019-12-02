012b50abd22eef029f14530c1d893876989495b3
Bart Kim <bart@eatple.com>
2019-12-02 17:36:56 +0000

    static file serving added

--- config/urls.py
+++ config/urls.py
@@ -63,3 +63,4 @@ urlpatterns = [
 
 # Media Link Url
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
