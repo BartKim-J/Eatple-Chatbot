707ad47e34890c00c2d2bc9d2e83b8ecfc5e9687
Bart Kim <bart@eatple.com>
2019-12-02 17:20:06 +0000

    uWSGI service add

--- run_wsgi.sh
+++ run_wsgi.sh
@@ -1,1 +1,5 @@
-sudo -u eatplus /home/ubuntu/.pyenv/versions/deploy_eatple/bin/uwsgi --http :8000 -ini /home/ubuntu/eatple.chatbot/.config_secret/uwsgi/deploy.ini
+sudo -u ubuntu /home/ubuntu/.pyenv/versions/deploy_eatple/bin/uwsgi --http :8000 -i /home/ubuntu/eatple-chatbot/.config_secret/uwsgi/deploy.ini
+
+# uwsgi --http :8000 --home /home/ubuntu/.pyenv/versions/deploy_eatple --chdir /home/ubuntu/eatple-chatbot -w config.wsgi.debug
+
+
