41fc83bcd363f264d55e6eb9041c022c100290c1
Bart Kim <bart@eatple.com>
2020-03-04 07:18:30 +0000

    filename change

--- schedule/status_update.sh
+++ schedule/status_update.sh
@@ -2,9 +2,9 @@
 cd /home/ubuntu/eatple-chatbot
 export DJANGO_SETTINGS_MODULE=config.settings.deploy
 
-echo Order Status Update Start
+echo Status Update Start
 
 /home/ubuntu/.pyenv/versions/deploy_eatple/bin/python /home/ubuntu/eatple-chatbot/manage.py order --update-all
 /home/ubuntu/.pyenv/versions/deploy_eatple/bin/python /home/ubuntu/eatple-chatbot/manage.py menu --update-all
 
-echo Order Status Update Done!!
+echo Status Update Done!!
