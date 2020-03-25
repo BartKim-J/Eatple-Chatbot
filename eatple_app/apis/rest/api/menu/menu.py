4147949b6d03ff63bbce2714a019b7444296d12f
Bart Kim <bart@eatple.com>
2020-03-25 08:47:22 +0000

    store rest api id bug fixed

--- eatple_app/apis/rest/api/menu/menu.py
+++ eatple_app/apis/rest/api/menu/menu.py
@@ -47,15 +47,16 @@ class MenuViewSet(viewsets.ModelViewSet):
             response['error_msg'] = PARTNER_LOGIN_300_INVALID_CRN.message
 
         if(param_valid(store)):
-            filter.add(Q(store__store_id=store), filter.OR)
+            filter.add(Q(store__id=store), filter.AND)
 
         if(param_valid(id)):
-            filter.add(Q(menu_id=id), filter.OR)
+            filter.add(Q(menu_id=id), filter.AND)
 
         if(param_valid(name)):
-            filter.add(Q(name_contains=name), filter.OR)
+            filter.add(Q(name_contains=name), filter.AND)
 
         menuList = Menu.objects.filter(filter)
+
 
         response['menus'] = MenuSerializer(menuList, many=True).data
         response['error_code'] = 200
