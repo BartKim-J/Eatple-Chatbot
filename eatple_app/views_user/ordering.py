9adcdf09791f9a52d9d24d61f1770995c7a1cceb
Bart Kim <bart@eatple.com>
2020-04-09 07:21:10 +0000

    wording error fix

--- eatple_app/views_user/ordering.py
+++ eatple_app/views_user/ordering.py
@@ -891,7 +891,7 @@ def kakaoView_PickupTime(kakaoPayload):
         return errorView('잘못된 주문 시간', '정상적인 주문 시간대가 아닙니다.')
     elif currentSellingTime == SELLING_TIME_DINNER:
         KakaoInstantForm().Message(
-            '오늘 점심은 이미 감되었어요.',
+            '오늘 점심은 이미 마감되었어요.',
             '내일 점심은 오늘 오후 4시부터 내일 오전 10시 30분 까지 주문하실 수 있어요.',
             kakaoForm=kakaoForm
         )
