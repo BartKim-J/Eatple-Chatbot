5e8bfa7b4a42903e52171eec70121fe6419f4957
Bart Kim <bart@eatple.com>
2020-04-23 05:32:09 +0000

    dashboard render bug fixed

--- eatple_app/views_admin/login.py
+++ eatple_app/views_admin/login.py
@@ -6,7 +6,7 @@ from eatple_app.forms import LoginForm
 
 def POST_AdminLogin(request):  # 로그인 기능
     if request.method == "GET":
-        return render(request, '/dashboard/base.html')
+        return render(request, 'dashboard/base.html')
 
     elif request.method == "POST":
         form = LoginForm(request.POST)
@@ -18,4 +18,4 @@ def POST_AdminLogin(request):  # 로그인 기능
             login(request, user=user_instance)
             return redirect('index')
         else:
-            return render(request, '/dashboard/base.html', {'f': form, 'error': '아이디나 비밀번호가 일치하지 않습니다.'})
+            return render(request, 'dashboard/base.html', {'f': form, 'error': '아이디나 비밀번호가 일치하지 않습니다.'})
